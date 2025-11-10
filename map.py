d84316ab721c6c9c92b18c29f80709fb1e0f1853
rebackjoseph28 <reback.joseph28@gmail.com>
2025-11-10 10:53:19 -0700

    now mapped to the actual building coordinates

--- map.py
+++ map.py
@@ -1,44 +1,67 @@
 from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem
 from PyQt6.QtGui import QPixmap, QBrush, QColor
-from PyQt6.QtCore import QTimer, QRectF
+from PyQt6.QtCore import QTimer
 import sys, random
 
-class SimpleMap(QGraphicsView):
+class GPSMapViewer(QGraphicsView):
     def __init__(self, map_path, bbox):
         super().__init__()
         self.scene = QGraphicsScene()
         self.setScene(self.scene)
+
+        # import map image
         self.map_pixmap = QPixmap(map_path)
         self.scene.addPixmap(self.map_pixmap)
 
-        self.bbox = bbox  # (min_lat, max_lat, min_lon, max_lon)
+        # bounding box
+        # (min_lat, max_lat, min_lon, max_lon)
+        self.bbox = bbox
+
+        # GPS marker
         self.marker = QGraphicsEllipseItem(-5, -5, 10, 10)
         self.marker.setBrush(QBrush(QColor("red")))
         self.scene.addItem(self.marker)
 
+        # 40.57579702201656, -105.08348366394911
+        self.lat = 40.57579702201656
+        self.lon = -105.08348366394911
+        self.update_marker()
+
+        # simulated gps updates
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_position)
         self.timer.start(1000)
 
-        self.lat, self.lon = (bbox[0] + bbox[1])/2, (bbox[2] + bbox[3])/2
-
-    def update_position(self):
-        # Simulate GPS drift
-        self.lat += random.uniform(-0.0005, 0.0005)
-        self.lon += random.uniform(-0.0005, 0.0005)
-        self.update_marker()
-
     def geo_to_scene(self, lat, lon):
-        # Convert geo coords to scene coords (simple linear map)
-        x = (lon - self.bbox[2]) / (self.bbox[3] - self.bbox[2]) * self.map_pixmap.width()
-        y = (self.bbox[1] - lat) / (self.bbox[1] - self.bbox[0]) * self.map_pixmap.height()
+        min_lat, max_lat, min_lon, max_lon = self.bbox
+        width = self.map_pixmap.width()
+        height = self.map_pixmap.height()
+
+        # GPS to pixel coordinates
+        x = (lon - min_lon) / (max_lon - min_lon) * width
+        y = (max_lat - lat) / (max_lat - min_lat) * height
         return x, y
 
     def update_marker(self):
         x, y = self.geo_to_scene(self.lat, self.lon)
         self.marker.setPos(x, y)
 
-app = QApplication(sys.argv)
-view = SimpleMap("bldg-map.png", bbox=(37.7, 37.8, -122.5, -122.3))
-view.show()
-sys.exit(app.exec())
+    def update_position(self):
+        # simulate small random movement
+        self.lat += random.uniform(-0.000003, 0.000003)
+        self.lon += random.uniform(-0.000003, 0.000003)
+        self.update_marker()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # From image - (min_lat, max_lat, min_lon, max_lon)
+    # Top right 40.575920, -105.084196
+    # Bottom left 40.575243, -105.082178
+
+    bbox = (40.575243, 40.575920, -105.084196, -105.082178)
+
+    view = GPSMapViewer("bldg-map.png", bbox)
+    view.show()
+    sys.exit(app.exec())
