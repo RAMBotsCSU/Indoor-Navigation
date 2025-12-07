9016958752575945933e1ffd8c270dacfddd6f03
rebackjoseph28 <reback.joseph28@gmail.com>
2025-12-06 19:32:11 -0700

    better gps code

--- gps.py
+++ gps.py
@@ -1,62 +1,51 @@
-#! /usr/bin/python
-# Written by Dan Mandle http://dan.mandle.me September 2012
-# License: GPL 2.0
-
+#!/usr/bin/env python3
 import os
-from gps import *
-from time import *
 import time
 import threading
+from gps import *
 
-gpsd = None #seting the global variable
+gpsd = None  # Global GPS object
 
-os.system('clear') #clear the terminal (optional)
 
 class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd
+        gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
+        self.running = True
 
-  def run(self):
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+    def run(self):
+        global gpsd
+        while self.running:
+            gpsd.next()  # Grab the next GPS packet
 
-if __name__ == '__main__':
-  gpsp = GpsPoller() # create the thread
-  try:
-    gpsp.start() # start it up
-    while True:
-      #It may take a second or two to get good data
-      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
 
-      os.system('clear')
+if __name__ == "__main__":
+    gpsp = GpsPoller()
+    gpsp.start()
 
-      print()
-      print(' GPS reading')
-      print('----------------------------------------')
-      print('latitude    ' , gpsd.fix.latitude)
-      print('longitude   ' , gpsd.fix.longitude)
-      print('time utc    ' , gpsd.utc,' + ', gpsd.fix.time)
-      print('altitude (m)' , gpsd.fix.altitude)
-      print('eps         ' , gpsd.fix.eps)
-      print('epx         ' , gpsd.fix.epx)
-      print('epv         ' , gpsd.fix.epv)
-      print('ept         ' , gpsd.fix.ept)
-      print('speed (m/s) ' , gpsd.fix.speed)
-      print('climb       ' , gpsd.fix.climb)
-      print('track       ' , gpsd.fix.track)
-      print('mode        ' , gpsd.fix.mode)
-      print()
-      print('sats        ' , gpsd.satellites)
+    try:
+        while True:
+            os.system("clear")
 
-      time.sleep(500) #set to whatever
+            print("")
+            print(" GPS Reading")
+            print("----------------------------------------")
+            print("Latitude:     ", gpsd.fix.latitude)
+            print("Longitude:    ", gpsd.fix.longitude)
+            print("Altitude (m): ", gpsd.fix.altitude)
+            print("Speed (m/s):  ", gpsd.fix.speed)
+            print("Track:        ", gpsd.fix.track)
+            print("Climb:        ", gpsd.fix.climb)
+            print("Mode:         ", gpsd.fix.mode)
 
-  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-    print("\nKilling Thread...")
-    gpsp.running = False
-    gpsp.join() # wait for the thread to finish what it's doing
-  print("Done.\nExiting.")
+            # Time handling
+            print("Time UTC:     ", gpsd.utc, "+", gpsd.fix.time)
+
+            # Satellites (depends on gpsd version)
+            try:
+                print("Satellites:   ", len(gpsd.satellites))
+            except:
+                print("Satellites:   unavailable")
+
+            time.sleep(1)  # Update every seco
