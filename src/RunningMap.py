74f7290a879cc633ae56644e1cf3ee28204829a1
rebackjoseph28 <reback.joseph28@gmail.com>
2025-12-15 08:38:50 -0700

    ?

--- src/RunningMap.py
+++ src/RunningMap.py
@@ -30,7 +30,7 @@ class RunningMap:
         """Convert world coordinates to grid coordinates."""
         cx = self.grid_size // 2
         cy = self.grid_size // 2
-        gx = int(cx - wx / self.cell_size_cm)
+        gx = int(cx + wx / self.cell_size_cm)
         gy = int(cy - wy / self.cell_size_cm)
         return gx, gy
 
